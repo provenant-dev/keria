935f686a4aa2114dc7dd22b24669665bbdbd6c8f
Arshdeep <arsh.binny@gmail.com>
2025-03-14 23:14:35 +0530

    updated migrate command

--- src/keria/app/cli/commands/migrate.py
+++ src/keria/app/cli/commands/migrate.py
@@ -7,49 +7,50 @@ keria.cli.keria.commands module
 import argparse
 
 from hio.base import doing
+from keri.app.cli.commands.migrate import run
 from keria.db import basing
 
-from hio.base import doing
-from keri import help, kering
-from keri.db import basing
-from keria.db import basing as abase
 
-logger = help.ogler.getLogger()
+def handler(args):
+    """
+    Migrate KERIA agents
 
-parser = argparse.ArgumentParser(description='Migrates for all agents')
-parser.set_defaults(handler=lambda args: handler(args),
+    Args:
+        args(Namespace): arguments object from command line
+    """
+    migrator = MigrateDoDoer(args)
+    return [migrator]
+
+
+parser = argparse.ArgumentParser(description='Runs outstanding migrations for all agents')
+parser.set_defaults(handler=handler,
                     transferable=True)
 parser.add_argument('--base', '-b', help='additional optional prefix to file location of KERI keystore',
                     required=False, default="")
 
-def handler(args):
-    kwa = dict(args=args)
-    return [doing.doify(migrate, **kwa)]
 
+class MigrateDoDoer(doing.DoDoer):
+    def __init__(self, args):
+        self.args = args
+        self.adb = basing.AgencyBaser(name="TheAgency", base=args.base, reopen=True, temp=False)
+        doers = [doing.doify(self.migrateAgentsDo)]
 
+        super(MigrateDoDoer, self).__init__(doers=doers)
 
-def migrate(tymth, tock=0.0, **opts):
-    _ = (yield tock)
-    args = opts["args"]
+    def migrateAgentsDo(self, tymth, tock=0.0):
+        """ For each agent in the agency, add a migrate doer
 
-    adb = abase.AgencyBaser(name="TheAgency", base=args.base, reopen=True, temp=False)
+        Parameters:
+            tymth (function): injected function wrapper closure returned by .tymen() of
+                Tymist instance. Calling tymth() returns associated Tymist .tyme.
+            tock (float): injected initial tock value
 
-    caids = []
-    for ((caid,), _) in adb.agnt.getItemIter():
-        caids.append(caid)
-    
-    for caid in caids:
-        print(f"Starting migration of agent {caid}...")
-        db = basing.Baser(name=caid,
-                          base=args.base,
-                          temp=False,
-                          reopen=False)
-        try:
-            db.reopen()
-        except kering.DatabaseError as ex:
-            print(f"\t skipping {caid}, unable reopen DB - {ex}")
-            continue
+        """
+        self.wind(tymth)
+        self.tock = tock
+        _ = (yield self.tock)
 
-        print(f"\t Migrating {caid}...")
-        db.migrate()
-        print(f"\t Finished migrating {caid}")
+        for ((caid,), _) in self.adb.agnt.getItemIter():
+            args = run.parser.parse_args(['--name', caid, '--base', self.args.base, '--temp', False])
+            self.extend([run.MigrateDoer(args)])
+        return True
